113d75ec6e43ec760d7d2a081adb3f4fa91573d2
kangrae <kangrae0501@gmail.com>
2025-12-30 20:45:02 +0900

    feat: AppConfig 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.config.AppConfig;
+import christmas.controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        Controller controller = appConfig.controller();
+        controller.run();
     }
 }
---
+++ src/main/java/christmas/controller/Controller.java
@@ -0,0 +1,20 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class Controller {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Controller(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+    
+    public void run() {
+
+    }
+
+}
