e8925d9ef49cdb21460a2c115a2a6335d9cdc710
kangrae <kangrae0501@gmail.com>
2025-12-30 20:33:34 +0900

    fix: 크리스마스 이후에는 크리스마스 할인이 적용되지 않는 것으로 변경

--- src/main/java/christmas/domain/discount/ChristmasDiscount.java
+++ src/main/java/christmas/domain/discount/ChristmasDiscount.java
@@ -4,11 +4,15 @@ import java.time.LocalDate;
 
 public class ChristmasDiscount {
 
+    private final static LocalDate CHRISTMAS = LocalDate.of(2023, 12, 25);
     private final static int INIT_DISCOUNT_MONEY = 1_000;
     private final static int ADDITIONAL_DISCOUNT_MONEY = 100;
 
     public static int discount(LocalDate date) {
-        return INIT_DISCOUNT_MONEY + ADDITIONAL_DISCOUNT_MONEY * (date.getDayOfMonth() - 1);
+        if (date.isBefore(CHRISTMAS)) {
+            return INIT_DISCOUNT_MONEY + ADDITIONAL_DISCOUNT_MONEY * (date.getDayOfMonth() - 1);
+        }
+        return 0;
     }
 
 }
