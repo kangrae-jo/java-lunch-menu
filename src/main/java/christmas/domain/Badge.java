2139f70fa7b6d87705b49417850d53057b1f526c
kangrae <kangrae0501@gmail.com>
2025-12-31 14:04:22 +0900

    feat: 배지 enum을 반환하는 메서드 구현

--- src/main/java/christmas/domain/Badge.java
+++ src/main/java/christmas/domain/Badge.java
@@ -2,14 +2,26 @@ package christmas.domain;
 
 public enum Badge {
 
-    STAR(5_000),
-    TREE(10_000),
-    SANTA(20_000);
+    SANTA("산타", 20_000),
+    TREE("트리", 10_000),
+    STAR("별", 5_000),
+    NONE("없음", 0);
 
+    private final String korean;
     private final int threshold;
 
-    Badge(int threshold) {
+    Badge(String korean, int threshold) {
+        this.korean = korean;
         this.threshold = threshold;
+    }
+
+    public static Badge from(int discountAmount) {
+        for (Badge badge : Badge.values()) {
+            if (badge.threshold >= discountAmount) {
+                return badge;
+            }
+        }
+        return NONE;
     }
 
 }
