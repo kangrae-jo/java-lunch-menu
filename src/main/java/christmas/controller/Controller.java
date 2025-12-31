bba6d4910cf589aaf48fe1c954dc5a97daf2801a
kangrae <kangrae0501@gmail.com>
2025-12-31 11:29:51 +0900

    feat: 재입력 메서드 추가

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -2,6 +2,8 @@ package christmas.controller;
 
 import christmas.view.InputView;
 import christmas.view.OutputView;
+import java.time.LocalDate;
+import java.util.function.Supplier;
 
 public class Controller {
 
@@ -14,9 +16,27 @@ public class Controller {
     }
 
     public void run() {
-        inputView.readDateOfVisit();
-        inputView.readOrderItems();
-        outputView.writeBenefitPreview();
+        LocalDate date = readDateOfVisit();
+        String orderItems = inputView.readOrderItems();
+
+        outputView.writeBenefitPreviewMsg();
+    }
+
+    private LocalDate readDateOfVisit() {
+        return retryUntilValid(() -> {
+            String date = inputView.readDateOfVisit();
+            return LocalDate.of(2023, 12, Integer.parseInt(date));
+        });
+    }
+
+    private <T> T retryUntilValid(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (Exception e) {
+                outputView.printErrorMsg(e.getMessage());
+            }
+        }
     }
 
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,8 +2,12 @@ package christmas.view;
 
 public class OutputView {
 
-    public void writeBenefitPreview() {
+    public void writeBenefitPreviewMsg() {
         System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+
+    public void printErrorMsg(String msg) {
+        System.out.println("[ERROR] " + msg);
     }
 
 }
