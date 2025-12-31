058c03f3b73ced7d9e8c9f974887a4b05cd1b2e6
kangrae <kangrae0501@gmail.com>
2025-12-31 13:56:02 +0900

    feat: 배지 enum 구현

---
+++ src/main/java/christmas/domain/Badge.java
@@ -0,0 +1,15 @@
+package christmas.domain;
+
+public enum Badge {
+
+    STAR(5_000),
+    TREE(10_000),
+    SANTA(20_000);
+
+    private final int threshold;
+
+    Badge(int threshold) {
+        this.threshold = threshold;
+    }
+
+}
